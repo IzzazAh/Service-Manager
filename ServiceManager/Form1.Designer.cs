5454e771a0214acc107fd885c63f204b5df1f4b9
IzzazAh <32457507+IzzazAh@users.noreply.github.com>
2017-10-02 21:18:43 +0530

    Add files via upload
    Version 1

---
+++ ServiceManager/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ServiceManager
+{
+    partial class Form1
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        this.textBox1 = new System.Windows.Forms.TextBox();
+        this.Query = new System.Windows.Forms.Button();
+        this.Restart = new System.Windows.Forms.Button();
+        this.SuspendLayout();
+        // 
+        // textBox1
+        // 
+        this.textBox1.Location = new System.Drawing.Point(211, 36);
+        this.textBox1.Multiline = true;
+        this.textBox1.Name = "textBox1";
+        this.textBox1.Size = new System.Drawing.Size(197, 108);
+        this.textBox1.TabIndex = 0;
+        this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+        // 
+        // Query
+        // 
+        this.Query.Location = new System.Drawing.Point(49, 36);
+        this.Query.Name = "Query";
+        this.Query.Size = new System.Drawing.Size(75, 23);
+        this.Query.TabIndex = 1;
+        this.Query.Text = "Query";
+        this.Query.UseVisualStyleBackColor = true;
+        this.Query.Click += new System.EventHandler(this.Query_Click);
+        // 
+        // Restart
+        // 
+        this.Restart.Location = new System.Drawing.Point(49, 121);
+        this.Restart.Name = "Restart";
+        this.Restart.Size = new System.Drawing.Size(75, 23);
+        this.Restart.TabIndex = 2;
+        this.Restart.Text = "Reset";
+        this.Restart.UseVisualStyleBackColor = true;
+        this.Restart.Click += new System.EventHandler(this.Restart_Click);
+        // 
+        // Form1
+        // 
+        this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.AutoScroll = true;
+        this.ClientSize = new System.Drawing.Size(442, 385);
+        this.Controls.Add(this.Restart);
+        this.Controls.Add(this.Query);
+        this.Controls.Add(this.textBox1);
+        this.Name = "Form1";
+        this.Text = "Service Manager";
+        this.ResumeLayout(false);
+        this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Button Query;
+    private System.Windows.Forms.Button Restart;
+}
+}
+
---
+++ ServiceManager/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+
+namespace ServiceManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            textBox1.Multiline = true;      //Multiline for Multiple Entries
+            textBox1.ScrollBars = ScrollBars.Vertical;      //Scrollable Space
+
+        }
+
+        private void Query_Click(object sender, EventArgs e)
+        {
+
+            string providerPath;
+            ConnectionOptions Options = new ConnectionOptions();
+            int top = 200;
+            int top2 = 160;
+
+           
+
+            string[] lines = textBox1.Lines;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+
+
+                int left = 50;
+                int left2 = 140;
+
+                string s = lines[i];
+
+                Button butto = new Button();
+                butto.Width = 200;
+                butto.Text = s;
+
+                butto.Top = top2;
+                butto.Left = 50;
+                this.Controls.Add(butto);
+
+
+
+
+                providerPath = String.Format(@"\\{0}\root\CIMv2", s);
+                ManagementScope scope = new ManagementScope(providerPath, Options);
+                try
+                {
+                    scope.Connect();
+                }
+                catch {
+                    Button button3 = new Button();
+                    button3.Left = left2;
+                    button3.Top = top;
+                    button3.Text = "Server is unreachable";
+                    button3.Width = 150;
+                    Controls.Add(button3);
+
+                    top += button3.Height + 52;
+                    top2 += button3.Height + 52;
+                    
+                    continue;
+                }
+
+
+                ObjectQuery query = new ObjectQuery("SELECT * FROM Win32_Service WHERE StartMode LIKE 'Auto' AND State = 'Stopped'");
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+
+                foreach (ManagementObject o in searcher.Get())
+
+                {
+                    string DisplayName = o["DisplayName"].ToString();
+                    string name = o["Name"].ToString();
+                    Button button = new Button();
+                    button.Left = left2;
+                    button.Top = top;
+                    button.Width = 200;
+                    button.Text = DisplayName;
+
+                    Button button2 = new Button();
+                    button2.Left = left;
+                    button2.Top = top;
+                    button2.Text = "Start";
+
+                    this.Controls.Add(button);
+                    this.Controls.Add(button2);
+
+                    top += button.Height + 2;
+                    top2 += button.Height + 2;
+                    button2.Click += new EventHandler(button2_Click);
+
+                    void button2_Click(object sender1, EventArgs e1)
+                    {
+                        
+                        ManagementPath path = new ManagementPath(string.Format("Win32_Service.Name='{0}'", name));
+                        ManagementObject obj = new ManagementObject(scope, path, new ObjectGetOptions());
+                        ManagementBaseObject outParams = obj.InvokeMethod("StartService", (ManagementBaseObject)null, null);
+                        uint returnCode = System.Convert.ToUInt32(outParams.Properties["ReturnValue"].Value);
+                    }
+                }
+
+                top += 50;
+                top2 += 50;
+
+                
+            }
+
+        }
+
+        private void Restart_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
